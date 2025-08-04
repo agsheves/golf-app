c8c378655424a68f70fa096f01d98ed3596e36b4
code_5@icloud.com <code_5@icloud.com>
2025-08-04 19:09:07 +0000

    Edited settings and ServerModule1

--- server_code/ServerModule1.py
+++ server_code/ServerModule1.py
@@ -5,6 +5,8 @@ import anvil.tables.query as q
 from anvil.tables import app_tables
 import anvil.server
 from datetime import datetime
+import anvil.secrets
+import anvil.users
 
 
 @anvil.server.callable
@@ -37,3 +39,40 @@ def delete_entry(entry):
     entry.delete()
   else:
     raise Exception("Entry does not exist")
+
+    import anvil.google.auth, anvil.google.drive, anvil.google.mail
+
+
+@anvil.server.callable
+def update_courses():
+  golf_files = app_files.golf_corse_registry
+  ws = golf_files["Sheet1"]
+  for r in ws.rows:
+    name = r['name']
+    address = r['address']
+    phone_number = r['phone_number']
+    thumbnail = ['thumbnail']
+    description = ['description']
+    website = ['website']
+    type = ['type']
+    cost = ['cost']
+    booking_window_days = ['booking_window_days']
+    scorecard = ['scorecard']
+    rating_google = ['rating_google']
+    rating_grint = ['rating_grint']
+    thumbnail = r['thumbnail']
+    booking_window_days = r['booking_window_days']
+    app_tables.course_info.add_row(
+      name=name,
+      address=address,
+      phone_number=phone_number,
+      thumbnail =	thumbnail,
+      description=description,
+      website=website,
+      type=type,
+      cost=cost,
+      booking_window_days=booking_window_days,
+      scorecard=scorecard,
+      rating_google=rating_google,
+      rating_grint=rating_grint
+    )
